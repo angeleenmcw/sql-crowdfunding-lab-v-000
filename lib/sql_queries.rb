02364cb18301eb117e0706faca113030a4ec6c24
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-29 05:42:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,10 +25,10 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, users.age, pledges.amount
-  FROM users
+  "SELECT projects.title, projects.funding_goal, pledges.amount
+  FROM projects
   INNER JOIN pledges
-  ON users.id = pledges.user_id 
+  ON projects.id = pledges.user_id 
   WHERE users.id = pledges.user_id
   ORDER BY users.name ASC;"
 end
