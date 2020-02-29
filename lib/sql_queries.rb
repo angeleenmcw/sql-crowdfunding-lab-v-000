2e4e022963335988bdc08b20034af1c4776aca68
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-29 07:34:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -54,7 +54,7 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, pledges.amount
   From projects
-  INNER JOIN pledges
+  JOIN pledges
   On projects.id = pledges.project_id
   WHERE projects.category = 'books';"
 end
