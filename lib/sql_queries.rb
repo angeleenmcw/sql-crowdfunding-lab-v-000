300e54debd40e9f50f00f595cc6e37b8400efd9c
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-29 07:20:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   LEFT JOIN pledges
   ON projects.id = pledges.project_id 
   WHERE projects.id = pledges.project_id
-  GROUP BY projects.title
+  GROUP BY projects.id
   ORDER BY projects.title ASC;"
 end
 
