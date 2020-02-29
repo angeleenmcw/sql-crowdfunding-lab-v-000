12bd74b4ed7166f8f0d65f96b473dac84cb6f8e6
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-29 06:11:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,8 +25,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
- "SELECT project.titles, project.funding_goal, pledges.amount, project.funding_goal - pledges.amount as Difference
-FROM Sets X
+ "SELECT project.titles, project.funding_goal, pledges.amount, project.funding_goal
+  pledges.amount as Difference
+  FROM projects
 LEFT OUTER JOIN SetY Y
   ON Y.id = X.ID
   "SELECT projects.title, projects.funding_goal, pledges.amount
