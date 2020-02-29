9b894fc29b241522252874a133d8458e894e51de
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-29 07:37:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
   FROM projects
-  LEFT OUTER JOIN pledges
+  LEFT JOIN pledges
   ON project_id = pledges.project_id
   GROUP BY projects.title
   HAVING SUM(pledges.amount >= projects.funding_goal);"
